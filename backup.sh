97b8915f852c1944fdd48e8b8bfc831faa1728fd
Sascha Hartl <sascha.hartl@stud.fh-campuswien.ac.at>
2025-03-23 20:10:56 +0100

    file and directory count added

--- backup.sh
+++ backup.sh
@@ -12,11 +12,26 @@ echo "Enter directory to backup"
 read backupDirectory
 
 createBackup(){
-# create a archive of the given directory and print the file information
-    tar -czvf $outputArchive $backupDirectory 2> /dev/null
-    echo "backup created and stored in ${outputDirectory} as ${outputFile}"
-    echo "ls ${outputDirectory}"
+    # count files before backup
+    files=$(nrFiles $backupDirectory)
+    dirs=$(nrDirectories $backupDirectory)
+
+    # create a archive of the given directory and print the file information
+    tar -czvf $outputArchive $backupDirectory
+    echo "Backup from ${backupDirectory} with ${files} files and ${dirs} directories created and stored in ${outputDirectory} as ${outputFile}"
+    echo "ls ${outputArchive}"
     ls -la $outputArchive
+
+
+}
+
+# functions from 2
+nrFiles() {
+    find "$1" -type f | wc -l 2> /dev/null
+}
+
+nrDirectories() {
+    find "$1" -type d | wc -l 2> /dev/null
 }
 
 createBackup
